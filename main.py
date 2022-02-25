91c9b10533f4e85b77146a5f913ec64edeb2e913
Valery Efremov <valeriy131100@gmail.com>
2022-02-25 10:12:00 +0300

    Setup DialogFlow

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+
+from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import (Updater,
                           CommandHandler,
@@ -12,7 +15,27 @@ def start(update: Update, context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+    session_client = context.bot_data['dialogflow_session_client']
+    project_id = context.bot_data['dialogflow_project_id']
+
+    session = session_client.session_path(project_id, update.message.chat_id)
+
+    text_input = dialogflow.TextInput(
+        text=update.message.text,
+        language_code='ru-RU'
+    )
+
+    query_input = dialogflow.QueryInput(
+        text=text_input
+    )
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    update.message.reply_text(
+        response.query_result.fulfillment_text
+    )
 
 
 if __name__ == '__main__':
@@ -22,6 +45,15 @@ if __name__ == '__main__':
     updater = Updater(telegram_token)
 
     dispatcher = updater.dispatcher
+
+    bot_data = dispatcher.bot_data
+    bot_data['dialogflow_session_client'] = dialogflow.SessionsClient()
+
+    google_credentials_path = env.str('GOOGLE_APPLICATION_CREDENTIALS')
+
+    with open(google_credentials_path, 'r') as credentials_file:
+        credentials = json.load(credentials_file)
+        bot_data['dialogflow_project_id'] = credentials['project_id']
 
     dispatcher.add_handler(
         CommandHandler("start", start)

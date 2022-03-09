c8de370808e2927cd08aeb4a1115a61c04a35191
Valery Efremov <valeriy131100@gmail.com>
2022-03-09 10:24:05 +0300

    Move intent creation into function in load_intents

--- load_intents.py
+++ load_intents.py
@@ -5,6 +5,32 @@ from google.cloud import dialogflow
 import config
 
 
+def add_intent(display_name, questions, answer):
+    parent = dialogflow.AgentsClient.agent_path(
+        config.dialogflow_project_id
+    )
+    training_phrases = []
+    for training_phrases_part in questions:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part
+        )
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=[answer])
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message]
+    )
+
+    intents_client.create_intent(
+        request={'parent': parent, 'intent': intent}
+    )
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Load intents for DialogFlow'
@@ -21,30 +47,8 @@ if __name__ == '__main__':
         intents = json.load(intents_file)
 
     for display_name, intent_description in intents.items():
-        training_phrases_parts = intent_description['questions']
-        answer = [intent_description['answer']]
-
-        parent = dialogflow.AgentsClient.agent_path(
-            config.dialogflow_project_id
+        add_intent(
+            display_name,
+            intent_description['questions'],
+            intent_description['answer']
         )
-        training_phrases = []
-        for training_phrases_part in training_phrases_parts:
-            part = dialogflow.Intent.TrainingPhrase.Part(
-                text=training_phrases_part
-            )
-            training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
-            training_phrases.append(training_phrase)
-
-        text = dialogflow.Intent.Message.Text(text=answer)
-        message = dialogflow.Intent.Message(text=text)
-
-        intent = dialogflow.Intent(
-            display_name=display_name,
-            training_phrases=training_phrases,
-            messages=[message]
-        )
-
-        intents_client.create_intent(
-            request={'parent': parent, 'intent': intent}
-        )
-
